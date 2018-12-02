3691a6d24171e3a12c9f0310933c1e684a357b55
srivatsav raju kurapati <[valid -email]>
2018-12-01 19:45:24 -0600

    second commit

--- Raj.java
+++ Raj.java
@@ -1,5 +1,10 @@
 public class Raj{
+void printer(int a, int b){
+System.out.println(""+a+b);
+}
 public static void main(String[] args){
 System.out.println("Hello Git");
+Raj r=new Raj();
+r.printer(4,6);
 }
 }
